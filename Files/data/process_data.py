23a107070f96bf3ea3b0228d11b9f2a08733cf7f
Luís <otaviogaravaso@gmail.com>
2021-02-14 10:59:12 -0300

    Create Load and Clean Data in the 'process_data.py' file

--- Files/data/process_data.py
+++ Files/data/process_data.py
@@ -1,13 +1,40 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
-
+    
+    #Load Datasets
+    messages = pd.read_csv("messages.csv", usecols = ['id','message'])
+    categories = pd.read_csv("categories.csv", sep = ",")
+    
+    #Return merged Dataset
+    return pd.merge(messages, categories)
+    
 def clean_data(df):
-    pass
+    
+    # Create a dataframe of the 36 individual category columns
+    cat_index = df["id"] #index
+    messages = df.drop('categories', axis = 1) #message
+    cat_data = df["categories"].str.split(";", expand = True) #data
+    
+    #Since Data looks like:
+    # related-1, request-0, offer-0, aid_related-0, medical_help-0, ...
+    #The next lines split the data into labels and values
+    #The labels will then be converted into column names
+    row = cat_data.iloc[0] 
+    cat_colnames = row.str.split("-", expand = True)[0]
+    cat_data.columns = cat_colnames
+    
+    for column in cat_colnames:
+        # set each value to be the last character of the string
+        cat_data[column] = cat_data[column].str.split("-",expand = True)[1]
+    
+    #Recreate 'categories' DataFrame with proper index 
+    categories = cat_data.set_index(cat_index).reset_index()
 
+    #Return merged Dataset
+    return pd.merge(messages, categories).drop_duplicates()
 
 def save_data(df, database_filename):
     pass  
